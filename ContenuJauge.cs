570f7cb5061e44ddd55f965dfadf353bb0a55115
Billon <remib06@hotmail.fr>
2012-11-18 22:13:49 +0100

    Jauge
    fonctionnel et à integrer au code principal

---
+++ ContenuJauge.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace DragonTears
+{
+    class ContenuJauge : Jauge
+    {
+        Rectangle rectangle = new Rectangle(13,-51,196,50);
+
+
+        #region gestion
+        public void Update(int sante, int santeMax, Texture2D jauge, Vector2 position)
+        {
+            rectangle.Width = (int)(((float)sante / (float)santeMax) * ((float)jauge.Width));
+            rectangle.Y = (int)position.Y;
+
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Begin();
+            spritebatch.Draw(jaugeVide, rectangle, Color.White);
+            spritebatch.End();
+        }
+        #endregion gestion
+
+    }
+}
---
+++ Jauge.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+
+
+namespace DragonTears
+{
+    class Jauge
+    {
+        public Texture2D jaugeVide;
+        public Texture2D JaugeVide
+        {
+            get { return jaugeVide; }
+            set { jaugeVide = value; }
+        }
+
+        Vector2 position = new Vector2(10, -51);
+
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+
+        #region fonctions gestion
+        public void LoadContent(ContentManager content, string assetName)
+        {
+            jaugeVide = content.Load<Texture2D>(assetName);
+        }
+
+        public void Update(bool combat)
+        {
+            if (combat == true )
+            {
+                if(position.Y < 10)
+                {
+                    position.Y++;
+                }
+            }
+            else if (combat == false && position.Y > -51)
+            {
+                position.Y--;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(jaugeVide, position, Color.White);
+            spriteBatch.End();
+        }
+
+        #endregion fonctions gestion
+
+        
+    }
+}
